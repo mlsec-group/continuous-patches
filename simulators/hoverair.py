9c06ad5c5968bb2709246267fab7ea58044c84cb
phanfeld <pia.hanfeld@gmail.com>
2024-02-13 11:36:13 +0100

    updated plotting to get better overview of movement over time

--- simulators/hoverair.py
+++ simulators/hoverair.py
@@ -18,7 +18,8 @@ class HoverSim():
         # These parameters could later be used to calculate the realtive pose of the image as in the flying_adversarial_patch paper.
 
         action = np.array([(1 - scale_factor), ty, tx])
-        new_pose = self.pose + action * 0.5 # simulate reaction after 0.5 secs
+        # print(action)
+        new_pose = self.pose + action * 0.1 # simulate reaction after 0.5 secs
         new_pose += np.random.normal(0.0, 0.1, (3,))
         return new_pose
 
@@ -28,8 +29,10 @@ if __name__ == '__main__':
     hover = HoverSim()
     all_poses = []
     all_poses.append(hover.pose)
+
+    # check for random parameters
     for _ in range(1000):
-        scale_factor = np.random.uniform(0.01, 2.5)
+        scale_factor = np.random.uniform(0.01, 1.5)
         tx = np.random.normal()
         ty = np.random.normal()
         hover.pose = hover.sim_new_pose(scale_factor, tx, ty)
