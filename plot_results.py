ea22468eac7d1accae2ab8bb92c10d94c9f68fff
Pia Hanfeld <pia.hanfeld@gmail.com>
2025-02-10 12:14:51 +0100

    added visualization in meshcat

--- plot_results.py
+++ plot_results.py
@@ -5,6 +5,12 @@ import numpy as np
 
 from util import get_yaw
 
+import meshcat
+import meshcat.geometry as g
+import meshcat.transformations as tf
+from meshcat.animation import Animation
+
+
 # def plot_results(path, target_trajectory, optimized_trajectories):
 
 #     path = Path(path)
@@ -22,6 +28,28 @@ from util import get_yaw
 #         ax.legend()
 #         pdf.savefig(fig)
 #         plt.close(fig)
+
+# # copied from https://github.com/rdeits/meshcat-python/blob/master/src/meshcat/geometry.py#L83
+# # since the latest pip-version doesn't include it yet
+class Plane(g.Geometry):
+
+    def __init__(self, width=1, height=1, widthSegments=1, heightSegments=1):
+        super(Plane, self).__init__()
+        self.width = width
+        self.height = height
+        self.widthSegments = widthSegments
+        self.heightSegments = heightSegments
+
+    def lower(self, object_data):
+        return {
+            u"uuid": self.uuid,
+            u"type": u"PlaneGeometry",
+            u"width": self.width,
+            u"height": self.height,
+            u"widthSegments": self.widthSegments,
+            u"heightSegments": self.heightSegments,
+        }
+
 
 if __name__ == '__main__':
 
@@ -52,7 +80,7 @@ if __name__ == '__main__':
     axs[3].set_ylabel('Yaw (deg)')
     plt.tight_layout()
     plt.savefig('results/plot_pose_over_time.jpg', dpi=200)
-    plt.show()
+    # plt.show()
     plt.close()
 
 
@@ -68,5 +96,50 @@ if __name__ == '__main__':
     ax.set_ylim(-1, 1)
     ax.set_zlim(0, 1.5)
     plt.savefig('results/plot_trajectory_3d.jpg', dpi=200)
-    plt.show()
+    # plt.show()
     plt.close()
+
+    # visualize in meshcat
+    vis = meshcat.Visualizer()
+    # vis.open()
+
+    vis["/Cameras/default"].set_transform(
+        tf.translation_matrix([0, 0, 0]).dot(
+        tf.euler_matrix(0, np.radians(-30), -np.pi/2)))
+
+    vis["/Cameras/default/rotated/<object>"].set_transform(
+        tf.translation_matrix([1, 0, 0]))
+
+    vis["Quadrotor"].set_object(
+        g.StlMeshGeometry.from_file('data/cf2_assembly.stl'))
+
+    # vis["projector"].set_object(Plane())
+
+    # # # Little experiment to support hyperplanes specified by n and a; This doesn't include all corner cases yet
+    # n = np.array([0,0,1])
+    # a = -1
+
+    # xd = np.array([0,1,0])
+    # zd = n
+    # yd = np.cross(xd, zd)
+    # R = tf.identity_matrix()
+    # R[:3,0] = xd
+    # R[:3,1] = yd
+    # R[:3,2] = zd
+    # p = np.array([0,0,-a/n[2]])
+
+    anim = Animation()
+
+    for idx, timestamp in enumerate(timestamps):
+        with anim.at_frame(vis, timestamp) as frame:
+            frame["Quadrotor"].set_transform(
+                tf.translation_matrix([*positions[idx]]).dot(
+                    tf.quaternion_matrix(rotations[idx])))
+            # frame["projector"].set_transform(tf.translation_matrix(p).dot(R))
+        # time.sleep(0.1)
+    vis.set_animation(anim)
+    res = vis.static_html()
+    # save to a file
+    Path("results").mkdir(exist_ok=True)
+    with open(Path("results") / "meshcat_example.html", "w") as f:
+        f.write(res)
