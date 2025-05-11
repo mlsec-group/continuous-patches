e25274deb73abc80dac9eb48f2e584d93b04f501
Pia Hanfeld <pia.hanfeld@gmail.com>
2025-05-11 16:52:36 +0200

    optimization for yolov5 working

--- dataset.sh
+++ dataset.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#SBATCH -c 6 
-#SBATCH --gres=gpu:1 
-#SBATCH -p gpu
-#SBATCH --time=02:00:00
+#SBATCH --partition=gpu-2h
+#SBATCH --gpus-per-node=1
+#SBATCH --ntasks-per-node=2
 
-source /home/hanfeld/.yolopatches/bin/activate
-python src/attacks.py --file $1 --model $2
+# source /home/hanfeld/.yolopatches/bin/activate
+# python src/attacks.py --file $1 --model $2
+apptainer run --nv container.sif python attacks.py --file $1 --model $2
